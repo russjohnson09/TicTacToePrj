64204b85558d7dccb16b032bbe234052389ab1b0
russjohnson09 <russjohnson09@gmail.com>
2012-09-23 19:37:08 -0400

    update

---
+++ src/package1/SuperTicTacToePanel.java
@@ -0,0 +1,16 @@
+package package1;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class SuperTicTacToePanel {
+	private JButton[][] board;
+	private Cell[][] iBoard;
+	private JButton quitButton;
+	private ImageIcon xIcon;
+	private ImageIcon oIcon;
+	private ImageIcon emptyIcon;
+
+	private SuperTicTacToeGame game;
+
+}
